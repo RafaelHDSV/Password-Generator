daaa55f863ed564daf338acfaa09f8bf97870f61
Rafael Henrique de Sousa Vieira <81830452+RafaelHDSV@users.noreply.github.com>
2022-10-16 11:43:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var slider = document.getElementById('slider')
+var button = document.getElementById('button')
+var sizePassword = document.getElementById('value')
+var passwordResult = document.getElementById('password')
+var passwordSection = document.querySelector('.password_section')
+
+var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*"
+var newPassword = ""
+
+sizePassword.innerHTML = slider.value
+
+slider.oninput = function () {
+    sizePassword.innerHTML = this.value
+}
+
+function generatePassword() {
+    var password = ""
+
+    for (var i = 0, n = charset.length; i < slider.value; i++) {
+        password += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    passwordSection.classList.remove("hide")
+    passwordResult.innerHTML = password
+    newPassword = password
+}
+
+function copyPassword() {
+    navigator.clipboard.writeText(newPassword)
+}
