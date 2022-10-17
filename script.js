9894aeb91d2fe3d180945b289c21c1bbc12e7b5d
Rafael Henrique de Sousa Vieira <81830452+RafaelHDSV@users.noreply.github.com>
2022-10-17 07:04:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-var slider = document.getElementById('slider')
-var button = document.getElementById('button')
-var sizePassword = document.getElementById('value')
-var passwordResult = document.getElementById('password')
-var passwordSection = document.querySelector('.password_section')
-
-var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*"
-var newPassword = ""
+let slider = document.getElementById('slider')
+let button = document.getElementById('button')
+let sizePassword = document.getElementById('value')
+let passwordResult = document.getElementById('password')
+let passwordSection = document.querySelector('.password_section')
+let predefinedSection = document.querySelector('.predefined_text')
+let predefinedText = document.getElementById('predefinedText')
+let uppercase = document.getElementById('switch-shadow_1 uppercase')
+let symbols = document.getElementById('switch-shadow_2 symbols')
+let numbers = document.getElementById('switch-shadow_3 numbers')
+let text = document.getElementById('switch-shadow_4 text')
+let lowercaseCharset = "abcdefghijklmnopqrstuvwxyz"
+let uppercaseCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let symbolsCharset = "*&$#@!"
+let numbersCharset = "0123456789"
+let textCharset = ''
+let charset = ''
+let newPassword = ''
 
 sizePassword.innerHTML = slider.value
 
@@ -13,19 +23,61 @@ slider.oninput = function () {
     sizePassword.innerHTML = this.value
 }
 
-function generatePassword() {
-    var password = ""
+function generateCharset() {
+    charset = lowercaseCharset
 
-    for (var i = 0, n = charset.length; i < slider.value; i++) {
+    if (uppercase.checked) {
+        charset += uppercaseCharset
+    }
+
+    if (symbols.checked) {
+        charset += symbolsCharset
+    }
+
+    if (numbers.checked) {
+        charset += numbersCharset
+    }
+
+    if (text.checked) {
+        predefinedSection.classList.remove("hide")
+
+        if (predefinedText.value == '') {
+            passwordSection.classList.add("hide")
+            console.log(a);
+        } else {
+            textCharset = predefinedText.value
+        }
+
+
+    } else {
+        predefinedSection.classList.add("hide")
+        textCharset = ''
+        predefinedText.value = ''
+    }
+}
+
+function generatePassword() {
+    generateCharset()
+
+
+    let password = ""
+
+    for (let i = 0, n = charset.length; i < slider.value; i++) {
         password += charset.charAt(Math.floor(Math.random() * n))
     }
 
     passwordSection.classList.remove("hide")
-    passwordResult.innerHTML = password
-    newPassword = password
+
+    if (textCharset == '') {
+        passwordResult.innerHTML = password
+    } else {
+        passwordResult.innerHTML = textCharset + password
+    }
+
+    newPassword = passwordResult.textContent
 }
 
 function copyPassword() {
-    alert("Copiada Com Sucesso")
+    passwordResult.innerHTML = 'Copiado com Sucesso'
     navigator.clipboard.writeText(newPassword)
 }
